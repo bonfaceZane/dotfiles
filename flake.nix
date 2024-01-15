0cf471e478f1b55d7c7e8d344fdc9edbb69f2264
zaneObwoni <zaneobwoni@gmail.com>
2024-01-15 14:58:04 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.obwoni000 = import ./nixos/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ nixos/home.nix
@@ -0,0 +1,67 @@
+{
+  description = "My home Manager configuration of me";
+
+
+  # Example: Set a configuration based on the target system
+  programs.zsh.enable = config.system == "aarch64-darwin";
+
+  programs = {
+    # Define the programs you want to manage here
+    emacs = {
+      enable = true;
+      package = pkgs.emacs;
+    };
+  }: 
+
+  # Define your home directory structure, shell environment, etc.
+  home.packages = with pkgs; [
+      git
+      zellij
+      htop
+
+       # archives
+      zip
+      xz
+      unzip
+      p7zip
+
+      # nix related
+      #
+      # it provides the command `nom` works just like `nix`
+      # with more details log output
+      nix-output-monitor
+
+      # productivity
+      hugo # static site generator
+      glow # markdown previewer in terminal
+
+          btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+
+  ];
+
+    # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+
+    # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+
+    # Example: Set your user information
+  users.extraUsers.obwoni000 = {
+    createHome = true;
+    shell = "~/bin/zsh";
+  };
+    
+};
